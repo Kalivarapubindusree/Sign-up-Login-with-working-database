f205d089d4f48b0f54fccf7ca4f86c760ccb53d5
Kalivarapubindusree <114821855+Kalivarapubindusree@users.noreply.github.com>
2023-05-04 12:45:25 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `users` (
+ `id ` int PRIMARY KEY AUTOINCREMENT,
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `username` varchar(50) NOT NULL,
+ `email` varchar(50) NOT NULL,
+ `password` varchar(50) NOT NULL,
+ `trn_date` datetime NOT NULL,
+ PRIMARY KEY (`id`)
+ );
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body style="margin: 50px;">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>S.no</th>
+                <th>Member ID</th>
+                <th>Nominee Name</th>
+                <th>Mobile number</th>
+                <th>Email</th>
+                <th>Validity</th>
+            </tr>
+                <tr>
+    <td>1</td>
+    <td>F6004497</td>
+    <td>Mr. Sumahasan Singupuram </td>
+    <td>837456116</td>
+    <td>sumahasan@gvpcew.ac.in</td>
+    <td>2026-05-04</td>
+   </tr>
+              <tr>
+    <td>2</td>
+    <td>F6004498</td>
+    <td>Mr. Sumahasan Singupuram </td>
+    <td>9963444007</td>
+    <td>kpnaidu@gvpcew.ac.in</td>
+    <td>2027-05-04</td>
+   </tr>
+            <tr>
+    <td>3</td>
+    <td>F6004499</td>
+    <td>Mr. Ch.V.V.D.Prasad </td>
+    <td>8639026349</td>
+    <td>prasadchelluri@gvpcew.ac.in</td>
+    <td>2029-05-04</td>
+   </tr>
+
+        </tbody>
+    </table>
+</body>
+</html>
